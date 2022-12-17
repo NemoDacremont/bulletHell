26b21a75e169ff9b67c97b942698077dd63e0f50
Nemo D'ACREMONT <nemo.dacremont@gmail.com>
2022-12-17 14:00:05 +0100

    trcucs de poo

--- src/tests/tests2Pygame/main.py
+++ src/tests/tests2Pygame/main.py
@@ -4,6 +4,7 @@ import pygame
 WIN_SIZE = WIN_WIDTH, WIN_HEIGHT = 1280, 720
 ### Créé la fenêtre
 WIN = pygame.display.set_mode(WIN_SIZE)
+
 # Donne un nom à la fenêtre
 pygame.display.set_caption("un jeu")
 
--- src/tests/testsOOP/main.py
+++ src/tests/testsOOP/main.py
@@ -9,15 +9,43 @@ class Entity():
         return self.nom
 
 class Tasse(Entity):
-    def __init__(self, x: int, y: int):
+    EST_REMPLI = True
+    NEST_PAS_REMPLI = False
+
+
+    def __init__(self, proprietaire: str, estRempli: bool, x: int, y: int):
+        self.proprietaire = proprietaire
+        self.estRempli = estRempli
 
         # Fait appel au constructeur de Entity
         super(Tasse, self).__init__("Tasse", x, y)
 
+    def vider(self):
+        self.estRempli = Tasse.NEST_PAS_REMPLI
+
+    def remplir(self):
+        self.estRempli = Tasse.EST_REMPLI
+
+
+    def boire(self):
+        if self.estRempli:
+            print(f"{self.proprietaire} boit dans sa tasse")
+            self.vider()
+        else:
+            print(f"{self.proprietaire} est triste: sa tasse est vide")
+
+
+
+
 unTruc = Entity("le truc", 3, 4)
-maTasse = Tasse(0, 0)
+maTasse = Tasse("Nemo", Tasse.EST_REMPLI, 0, 0)
 
 print(unTruc.getNom())
 print(maTasse.getNom())
 
+maTasse.boire()
+maTasse.boire()
+
+maTasse.remplir()
+maTasse.boire()
 
