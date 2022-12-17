8120e3055988964a3b29bdb54414c84cc8aa75ed
Nemo D'ACREMONT <nemo.dacremont@gmail.com>
2022-12-17 14:00:09 +0100

    les plans

---
+++ plans/fichiers.mm
@@ -0,0 +1,139 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="/" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1671271020547">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#2e3440" zoom="0.455">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="file:/home/odasta/Mega/Cours/prepa-2/cartes_mentales/SI/Asservissement/Fondamental/Asservissement.mm" followedTemplateLocation="template:/dark_nord_template.mm" followedMapLastTime="1670489074000"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" MAX_WIDTH="10 cm" MIN_WIDTH="0 cm" VGAP_QUANTITY="20 pt" CHILD_NODES_ALIGNMENT="BY_CENTER">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Cascadia Code" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode TEXT="mdr" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b" MAX_WIDTH="100 cm">
+<font SIZE="16"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Cascadia Code" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="16"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="/assets/" POSITION="right" ID="ID_1823674294" CREATED="1671271022851" MODIFIED="1671271158023">
+<node TEXT="./images/" ID="ID_711540653" CREATED="1671271031155" MODIFIED="1671271160632">
+<node TEXT="./perso1/" ID="ID_240180241" CREATED="1671271120235" MODIFIED="1671271227017">
+<node TEXT="./perso1_1.png" ID="ID_1733533596" CREATED="1671271176724" MODIFIED="1671271239586"/>
+<node TEXT="./perso1_2.png" ID="ID_1490040702" CREATED="1671271177012" MODIFIED="1671271234439"/>
+<node TEXT="..." ID="ID_1327705758" CREATED="1671271197012" MODIFIED="1671271198985"/>
+</node>
+<node TEXT="./perso2/" ID="ID_1930455669" CREATED="1671271241308" MODIFIED="1671271246527">
+<node TEXT="..." ID="ID_1915683225" CREATED="1671271247676" MODIFIED="1671271250494"/>
+</node>
+<node TEXT="./perso..." ID="ID_1888138830" CREATED="1671271252636" MODIFIED="1671271256350"/>
+<node TEXT="./boss1/" ID="ID_1527694586" CREATED="1671271121756" MODIFIED="1671271175253">
+<node TEXT="./boss1_1.png" ID="ID_191688935" CREATED="1671271176724" MODIFIED="1671271189856"/>
+<node TEXT="./boss1_2.png" ID="ID_1637030452" CREATED="1671271177012" MODIFIED="1671271194672"/>
+<node TEXT="..." ID="ID_838694159" CREATED="1671271197012" MODIFIED="1671271198985"/>
+</node>
+<node TEXT="./boss2/" ID="ID_1384727644" CREATED="1671271258308" MODIFIED="1671271262697">
+<node TEXT="..." ID="ID_1058263464" CREATED="1671271264157" MODIFIED="1671271265342"/>
+</node>
+<node TEXT="..." ID="ID_1151315988" CREATED="1671271267749" MODIFIED="1671271270217"/>
+<node TEXT="./background.png" ID="ID_1417282008" CREATED="1671271200780" MODIFIED="1671271209984"/>
+</node>
+<node TEXT="./sons/" ID="ID_1171574520" CREATED="1671271031490" MODIFIED="1671271274714">
+<node TEXT="./musiques/" ID="ID_791824910" CREATED="1671271275845" MODIFIED="1671271286760">
+<node TEXT="./main_menu.wav" ID="ID_1475036316" CREATED="1671271304236" MODIFIED="1671271311342"/>
+<node TEXT="./boss1.wav" ID="ID_1258275153" CREATED="1671271312524" MODIFIED="1671271320391"/>
+<node TEXT="..." ID="ID_1323981415" CREATED="1671271331100" MODIFIED="1671271333753"/>
+</node>
+<node TEXT="./entites/" ID="ID_1084590430" CREATED="1671271288045" MODIFIED="1671271294359">
+<node TEXT="./attack.wav" ID="ID_190755386" CREATED="1671271295813" MODIFIED="1671271301561"/>
+<node TEXT="./hurt.wav" ID="ID_1863793051" CREATED="1671271302533" MODIFIED="1671271325359"/>
+<node TEXT="..." ID="ID_1989474257" CREATED="1671271327092" MODIFIED="1671271329174"/>
+</node>
+</node>
+</node>
+<node TEXT="./src" POSITION="right" ID="ID_1312375544" CREATED="1671271023108" MODIFIED="1671271338665">
+<node TEXT="./main.py" ID="ID_1660384584" CREATED="1671271339869" MODIFIED="1671271369768"/>
+<node TEXT="./jeu.py" ID="ID_1736584995" CREATED="1671271362485" MODIFIED="1671271373114"/>
+<node TEXT="./entites/" ID="ID_72721912" CREATED="1671271366437" MODIFIED="1671271380986">
+<node TEXT="./entite.py" ID="ID_538481415" CREATED="1671271410462" MODIFIED="1671271429090"/>
+<node TEXT="./joueur.py" ID="ID_1106955004" CREATED="1671271415470" MODIFIED="1671271426104"/>
+<node TEXT="./boss.py" ID="ID_976124524" CREATED="1671271432150" MODIFIED="1671271441344"/>
+</node>
+<node TEXT="./vues/" ID="ID_1578669588" CREATED="1671271383021" MODIFIED="1671271456602">
+<node TEXT="./vue.py" ID="ID_795810040" CREATED="1671271457654" MODIFIED="1671271463954"/>
+<node TEXT="./menuPrincipal.py" ID="ID_99583591" CREATED="1671271469638" MODIFIED="1671271480440"/>
+<node TEXT="./parametres.py" ID="ID_707788127" CREATED="1671271470894" MODIFIED="1671271486472"/>
+<node TEXT="./bosses/" ID="ID_1234417933" CREATED="1671271491598" MODIFIED="1671271508116">
+<node TEXT="./boss1.py" ID="ID_1811767560" CREATED="1671271509238" MODIFIED="1671271512987"/>
+<node TEXT="./boss2.py" ID="ID_1673903382" CREATED="1671271509238" MODIFIED="1671271528667"/>
+<node TEXT="..." ID="ID_1962488978" CREATED="1671271514238" MODIFIED="1671271532939"/>
+</node>
+</node>
+</node>
+</node>
+</map>
